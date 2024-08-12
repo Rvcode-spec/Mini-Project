7c8c2ef7f436d6c4bcd0ce23a0d1ec2e391c5c3a
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-08-13 00:15:04 +0530

    Stop Watch

---
+++ React Project/Mini-Project/stop-watch/src/watch.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function watch() {
+  return (
+    <div className='mini-box'>
+
+                <div className='watch-main'>
+                    <div className='watch-time'>
+
+                    </div>
+                </div>
+            
+            <div className='watch-button'>
+                    <button>Start</button>
+                    <button>Stop</button>
+                    <button>Reset</button>
+            </div>
+
+      
+    </div>
+  )
+}
