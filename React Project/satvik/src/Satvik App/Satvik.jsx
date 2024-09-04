62d61b070693367128bcffe1f4564631f3421d63
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-09-04 17:18:16 +0530

    Satvik App

---
+++ React Project/satvik/src/Satvik App/Satvik.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+export default function Index() {
+  return (
+    <div className='Header'>
+    
+        <div className='icon'>
+        <i className="fa-solid fa-bars" id='menuIcon'></i>
+            <div className='menu-bar'>
+        </div>
+    </div>      
+<div>
+                <h1>Satvik.in</h1>
+                <div className=''>
+
+            </div>
+        </div>
+
+      
+    </div>
+  )
+}
--- React Project/satvik/src/index.js
+++ React Project/satvik/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Index from './Satvik App/Satvik';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Index/>
     
+
   </React.StrictMode>
 );
+
+
 
 
