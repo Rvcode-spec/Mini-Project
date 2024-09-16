a4a60464cea1c60335f404ad92eb59b500afd8de
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-09-16 23:27:57 +0530

    Upadat on input

--- React Project/todowithcontextapi/src/Todocontext.jsx
+++ React Project/todowithcontextapi/src/Todocontext.jsx
@@ -2,15 +2,17 @@ import React, { createContext, useState } from 'react'
 
 const TodoApicontext=createContext();
 const Todoprovider = ({children}) => {
-    const [todo, setTodo]=useState([""]);
+    const [todo, settodo]=useState(['']);
     
     const addtodo=(task)=>{
-      setTodo([...todo,task])
+      settodo([...todo,task])
+      console.log('Todo added:', task);
+      
     }
     const removetodo=(index)=>{
         const newtodo=[...todo];
         newtodo.splice(index,1);
-        setTodo(newtodo)
+        settodo(newtodo)
     }
   return (
     <TodoApicontext.Provider value={{todo,addtodo,removetodo}}>
--- React Project/todowithcontextapi/src/Todocontextapi/Input.jsx
+++ React Project/todowithcontextapi/src/Todocontextapi/Input.jsx
@@ -3,14 +3,14 @@ import { TodoApicontext } from '../Todocontext'
 
 
 const Input = () => {
-  const {todo, settodo}=useContext('')
-  const { addtodo } = useContext(TodoApicontext);
+  // const {todo, settodo}=useContext('')
+  const { todo, settodo, addtodo} = useContext(TodoApicontext);
+  // const {addtodo}=useContext(TodoApicontext);
 
-  const handleAddTodo = () => {
-    if (todo.trim()) {    // trim() removes leading/trailing spaces, ensures todo is not empty
+  const AddTodo = () => {
       addtodo(todo);  // Call the addtodo function from context
       settodo('');    // Clear the input after adding the todo
-    }
+    
   };
   
   return (
@@ -18,8 +18,8 @@ const Input = () => {
       <input type="text"  placeholder='Enter the Todos'
       value={todo} 
       onChange={(e) => settodo(e.target.value)} />
-
-      <button onClick={handleAddTodo}>Add</button>
+      
+      <button onClick={AddTodo}>Add</button>
     </div>
   )
 }
