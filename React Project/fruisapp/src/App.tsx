629b12f9e5eb7058a6ece024985fbbbb907c4aff
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-09-18 16:31:33 +0530

    all file

--- React Project/fruisapp/src/App.tsx
+++ React Project/fruisapp/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Router from './Router/router';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
      <Router/>
-      
+      <Header/>
+      <Footer/>
     </div>
   );
 }
--- React Project/fruisapp/src/Router/router.tsx
+++ React Project/fruisapp/src/Router/router.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import Homepages from '../pages/homepages'
-import { Routes, Route } from 'react-router-dom'
+import { Routes,Route } from 'react-router-dom'
+
+import Cart from '../pages/Cart'
+import Shop from '../pages/Shop'
+import Checkout from '../pages/Checkout'
+import Shopdetails from '../pages/Shopdetails'
+import Thanks from '../pages/Thanks'
+import Contact from '../pages/Contact'
+import Homepage from '../pages/Homepage'
 
 const Router = () => {
   return (
-    <div>
-        <Routes>
-            <Route path="/" element={<Homepages/>}></Route>
-        </Routes>
-      
-    </div>
+    <Routes>
+<Route path="/" element={<Homepage/>}/>
+<Route path="/cart" element={<Cart/>}/>
+<Route path="/shop" element={<Shop/>}/>
+<Route path="/checkout" element={<Checkout/>}/>
+<Route path="/shopdetails" element={<Shopdetails/>}/>
+<Route path="/thanks" element={<Thanks/>}/>
+<Route path="/contact" element={<Contact/>}/>
+    </Routes>
   )
 }
 
