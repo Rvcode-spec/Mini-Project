fc100e92d649a894068b09bf87f498184c4ad95d
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-09-24 11:58:27 +0530

    Todos Add

--- React Project/todowithredux/src/Reducer/Reducer.js
+++ React Project/todowithredux/src/Reducer/Reducer.js
@@ -4,9 +4,16 @@ const initialState ={
     todos:['ravi' , 'shankar']
 }
 const todoReducer=(state=initialState,action)=>{
+// console.log("add ", action);
 
-    return state;
-
+switch(action.type){
+    case "ADD_TODO":
+        return {
+                
+                    todos:[...state.todos, action.payload]
+                };
+                default:
+                        return state; 
+    }
 }
-
 export default todoReducer;
--- React Project/todowithredux/src/Todo Component/Input.jsx
+++ React Project/todowithredux/src/Todo Component/Input.jsx
@@ -1,25 +1,65 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function Input() {
-    let [todo,setdodo]=useState('ravi');
+    const [todo, setTodo] = useState("");  // Corrected state variable and setter name
+    const[error, seterror]=useState(true)
+    const dispatch = useDispatch();  // Moved useDispatch to the top level
 
-    const changeinput =(e)=>{
+    const changeInput = (e) => {
         console.log(e.target.value);
-        setdodo(e.target.value)
     
-    }
-
-    const sumbit=(event)=>{
-        event.preventDefault();
+        // if(e.target.value.length>0){
+            setTodo(e.target.value)
+            seterror(false)
+           
+        //   }else{
+        //     seterror(true)
+        //     return;
+        //   }
         
     }
-  return (
-    <div>
-            <input type="text" placeholder='Enter the todo' onChange={changeinput} />
-            <button>Add No</button>
-    </div>
+       
 
-  )
+    const submit = (event) => {
+        event.preventDefault();
+        // Dispatching the action to add a new todo
+       if(todo!==""){
+        dispatch({
+            type: "ADD_TODO",
+            payload: todo
+        });
+        setTodo("")
+}else{
+    seterror(true);
+//  alert("HJJH")
+    
+} 
+
+     
+    };
+
+    return (
+        <div>
+            {/* Uncomment form tag if form is needed */}
+            {/* <form onSubmit={submit}> */}
+                <input 
+                    type="text" 
+                    placeholder='Enter the todo'
+                    onChange={changeInput}  // Fixed handler name
+                    value={todo}
+                />
+
+                
+                    {
+
+                        error &&  <p className='text text-danger'>Enter some todo to continue</p>
+                     }
+                
+                <button onClick={submit}>Add Todo</button>
+            {/* </form> */}
+        </div>
+    );
 }
 
-export default Input
+export default Input;
